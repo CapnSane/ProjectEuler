f6576102567f11b9c1f4827dbee531063c642de0
Leonardo Schneider <lnrdschneider@gmail.com>
2020-07-03 20:36:50 -0300

    Lowest palindrome calculus implementation and loop for optimization

--- problem4.py
+++ problem4.py
@@ -2,13 +2,17 @@ lista = {}
 
 def function(a):
     return a == a[::-1]
-for i in range(999,100,-1):
-    for j in range(999,100,-1):
+for i in range(999,101,-1):
+    for j in range(999,i-1,-1):
         if function(str(i*j)):
             lista[i*j] = (i,j)
-            break
+            # break
 
+min_key = min(lista)
 max_key = max(lista)
 
+print("The lowest palindrome made from the product of two 3-digit numbers is:", min_key)
+print("The two 3-digit numbers that make the lowest palindrome are:", lista[min_key])
+print("------------------------------------------------------------------------------")
 print("The largest palindrome made from the product of two 3-digit numbers is:", max_key)
-print("The two 3-digit numbers that make it are:", lista[max_key])
+print("The two 3-digit numbers that make the largest palindrome are:", lista[max_key])
