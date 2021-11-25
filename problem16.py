5702bf25d8fc52ee7ca2a6740970ea7d9cb853d5
Leonardo Schneider <lnrdschneider@gmail.com>
2021-11-25 19:39:23 -0300

    Problem title changed

--- problem16.py
+++ problem16.py
@@ -1,6 +1,11 @@
-# 2**15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+'''
+Problem 16
 
-# What is the sum of the digits of the number 2**1000?
+2**15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2**1000?
+
+'''
 
 exp = 1000
 x = 2**exp
