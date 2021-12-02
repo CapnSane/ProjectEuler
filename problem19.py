a11b25ab4cc8f5cffe8dff0b6d5ab0ee117866c1
Leonardo Schneider <lnrdschneider@gmail.com>
2021-12-02 15:45:18 -0300

    leap year and normal year listed

--- problem19.py
+++ problem19.py
@@ -15,20 +15,58 @@ You are given the following information, but you may prefer to do some research 
 
 How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 
-
 '''
 
-year = 365
-yearbi = 366
+jan = 31
+feb = 28
+mar = 31
+apr = 30
+may = 31
+jun = 30
+jul = 31
+aug = 31
+sep = 30
+okt = 31
+nov = 30
+dec = 31
 
-listy = []
-listbi = []
+months = [
+  jan, 
+  feb, 
+  mar, 
+  apr, 
+  may, 
+  jun, 
+  jul, 
+  aug, 
+  sep, 
+  okt, 
+  nov, 
+  dec
+]
 
-for i in range(1901, 2001):
-  if i % 4 == 0:
-    listbi.append(i)
-  else:
-    listy.append(i)
+def jorge():
+  listy = []
+  listbi = []
+  for i in range(1901, 2001):
+    if i % 4 == 0:
+      listbi.append(i)
+    else:
+      listy.append(i)
+  return listy, listbi
+
+
+
+print(jorge())
+# print(months)
+
+
+
+# totalbi = len(listbi)
+# total = len(listy)
+
+# sundays = (366*totalbi + 365*total)//7
 
 # print(listbi)
 # print(listy)
+# print(sundays)
